c39a9876cc3c5b075a7e19e2bf609d95573a9227
phmj <phmonnerat_jr@yahoo.com.br>
2023-09-14 09:04:49 -0300

    Add files via upload

--- app_do_excel.py
+++ app_do_excel.py
@@ -16,25 +16,25 @@ st.write(df1)
 # Método 1
 # ========
 
-buffer = io.BytesIO()
+#buffer = io.BytesIO()
 
-# Create a Pandas Excel writer using XlsxWriter as the engine.
-#with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
+## Create a Pandas Excel writer using XlsxWriter as the engine.
+##with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+#with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
 
-    # Write each dataframe to a different worksheet.
-    df1.to_excel(writer, sheet_name='Sheet1')
-    #df2.to_excel(writer, sheet_name='Sheet2')
+ #   # Write each dataframe to a different worksheet.
+ #   df1.to_excel(writer, sheet_name='Sheet1')
+ #   #df2.to_excel(writer, sheet_name='Sheet2')
 
-    # Close the Pandas Excel writer and output the Excel file to the buffer
-    writer.save()
+ #   # Close the Pandas Excel writer and output the Excel file to the buffer
+ #   writer.save()
 
-    st.download_button(
-        label="Download Excel worksheets - Método 1",
-        data=buffer,
-        file_name="pandas_multiple.xlsx",
-        mime="application/vnd.ms-excel"
-    )
+ #   st.download_button(
+ #       label="Download Excel worksheets - Método 1",
+ #       data=buffer,
+ #       file_name="pandas_multiple.xlsx",
+ #       mime="application/vnd.ms-excel"
+ #   )
 
 # ========
 # Método 2
