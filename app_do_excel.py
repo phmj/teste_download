3ff90b43580e09bcd1124e16cf8da377d16aeb48
phmj <phmonnerat_jr@yahoo.com.br>
2023-11-01 09:59:32 -0300

    Add files via upload

--- app_do_excel.py
+++ app_do_excel.py
@@ -40,6 +40,8 @@ st.write(df1)
 # Método 2
 # ========
 
+
+
 # Assuming 'data' is your DataFrame
 data = df1.copy()
 data.to_excel("output.xlsx", index=False)
@@ -54,7 +56,18 @@ if st.button("Download Excel File = Método 2"):
             key="excel-download",
         )
 
+import os
 
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+#st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
 
 
 
